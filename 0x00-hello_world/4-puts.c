7a10ff305b6f94e5039ff4705b30d672092b35e8
Darndy <godonudaniel@gmail.com>
2023-06-15 14:30:12 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programme that prints a specific line and ending with 0
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

ffcc68e66866cac251acc380795dfb7ac2b994b4
Darndy <godonudaniel@gmail.com>
2023-06-15 14:46:48 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that print a line of code without the function puts
+ * Return: o
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (o);
+}

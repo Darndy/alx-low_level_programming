f1146278107edd4f55fe341b0c0f1d331496a69a
Dandy <godonudaniel@gmail.com>
2023-07-28 10:40:22 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,13 @@
-global    main
-
-          extern    printf
-
+global main
+extern    printf
 main:
 
-          mov   edi, format
+mov edi, format
+xor   eax, eax
 
-          xor   eax, eax
-
-          call  printf
-
-          mov         eax, 0
-
-          ret
-
-format: db `Hello, Holberton\n`,0
+call  printf
+mov eax, 0
+ret
+format: db`Hello, Holberton\n`,0
 
 

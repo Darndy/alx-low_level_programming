802313d5453be8cb826056086d1e2332c51d5bbf
Dandy <godonudaniel@gmail.com>
2023-07-28 10:21:07 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
-
+/**
+* first - Pirints sentence Prior To The Main
+* function is being executed
+*/
 void first(void) __attribute__ ((constructor));
 
-
-/**
-* first - prints a sentence before the main
-* function is executed
-*/
-
 void first(void)
-
 {
-
 printf("You're beat! and yet, you must allow,\n");
-
 printf("I bore my house upon my back!\n");
 
 }
